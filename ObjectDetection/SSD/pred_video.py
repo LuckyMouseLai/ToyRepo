340f971cdae562122fdeda338f5f4928e4b34bce
lzq <6269947161@qq.com>
2022-08-17 21:30:11 +0800

    add windows adaptation setting

--- ObjectDetection/SSD/pred_video.py
+++ ObjectDetection/SSD/pred_video.py
@@ -34,6 +34,8 @@ def read_camera(cfg, args):
         # print(new_frame.shape)
         cv2.putText(new_frame, "FPS: {0}".format(float('%.1f' % (count / (time.time() - start_time)))), (20, 20),
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+        cv2.namedWindow('window', cv2.WINDOW_FREERATIO)  # 设置自适应窗口
+        cv2.resizeWindow("window", 960, 720)  # 设置显示窗口大小
         cv2.imshow('window', new_frame)  # 可以通过resize frame的大小，达到修改窗口大小的效果
         key = cv2.waitKey(1)  # 检测按键时间，如果按键，返回键值ASCII否则返回-1。如果参数为0，直到按键才继续
         if key == ord('q'):
